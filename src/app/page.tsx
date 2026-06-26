152e53adc756a69c5e70ebfce818b8ac8be88062
Hikayat Journal <muhammad.titian@gmail.com>
2026-06-27 05:44:11 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import Navbar from "@/components/ui/Navbar";
+import HeroSection from "@/components/sections/HeroSection";
+import PremissSection from "@/components/sections/PremissSection";
+import {
+  StatsSection,
+  EventSection,
+  GallerySection,
+  MembershipSection,
+  FAQSection,
+  CTASection,
+  Footer,
+} from "@/components/sections/OtherSections";
+
+export default function HomePage() {
+  return (
+    <main>
+      <Navbar />
+      <HeroSection />
+      <PremissSection />
+      <StatsSection />
+      <EventSection />
+      <GallerySection />
+      <MembershipSection />
+      <FAQSection />
+      <CTASection />
+      <Footer />
+    </main>
+  );
+}
